076f189fad32d4d006f160ead6dc21432f2b4b5c
adshrink <77864941+adshrink@users.noreply.github.com>
2022-07-05 19:03:06 +0200

    Update skip.react.js

--- skip.react.js
+++ skip.react.js
@@ -29,7 +29,7 @@ if(hasPersonalizedAdsConsent&&hasPersonalizedProfileConsent){// Set request non-
 let isBlocked;const tryRequest=async()=>{try{return fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",{method:'HEAD',mode:'no-cors'})).then(function(response){// Google Ads request succeeded, so likely no ad blocker
 isBlocked=false;return isBlocked;}).catch(function(e){// Request failed, likely due to ad blocker
 isBlocked=true;return isBlocked;});}catch(error){// fetch API error; possible fetch not supported (old browser)
-// Marking as a blocker since there was an error and so
+// Marking as a blocker since there was an error and so 
 // we can prevent continued requests when this function is run
 console.log('checkAdBlocker',error);isBlocked=true;return isBlocked;}};return isBlocked!==undefined?isBlocked:await tryRequest();};function _hasPopupBlocker(poppedWindow){var result=false;try{if(typeof poppedWindow=='undefined'){// Safari with popup blocker... leaves the popup window handle undefined
 result=true;}else if(poppedWindow&&poppedWindow.closed){// This happens if the user opens and closes the client window...
